ba90d42dc94ea9cfb3bec0bbd45996f23a956f0a
Rafael <rgomcab714a@ieslacampina.es>
2023-06-08 21:05:19 +0200

    Actualización automática

--- crearCombate2.php
+++ crearCombate2.php
@@ -17,13 +17,14 @@
 		//Guardamos las variables de nuestro formulario
         $idpk1=$_POST['contrincante1'];
         $idpk2=$_POST['contrincante2'];
-        $pokemon1="";
-        $pokemon2="";
         $fecha=$_POST['fecha'];
         //inicializamos esta variable
         $poder1=0;
         $poder2=0;
-
+        $victorias1=0;
+        $victorias2=0;
+        $derrotas1=0;
+        $derrotas2=0;
         //Añadimos la excepción de si los dos contrincantes fueran el mismo
         if($idpk1==$idpk2){
             echo "<p class=' alert alert-danger'>Error al insertar. No puede combatirse a si mismo</p>";
@@ -41,24 +42,18 @@
             while($fila=$cons1->fetch_assoc()){
                 $poder1= $fila['ataque'] + $fila['defensa'] + $fila['velocidad'] + ($fila['Nivel']/4);
                 $pokemon1= $fila['nombre'];
+                $victorias1=$fila['combates_ganados'];
+                $derrotas1=$fila['combates_perdidos'];
             }
+
+            
             while($fila2=$cons2->fetch_assoc()){
                 $poder2= $fila2['ataque'] + $fila2['defensa'] + $fila2['velocidad'] + ($fila2['Nivel']/4);
                 $pokemon2= $fila2['nombre'];
+                $victorias2=$fila2['combates_ganados'];
+                $derrotas2=$fila2['combates_perdidos'];
             }
 
-            //Obtenemos las victorias y derrotas que tienen los pokemon participantes
-            $vicpoke1 = "select combates_ganados from pokemon where nombre like '$pokemon1'";
-            $resvicPoke1= $mysqli->query($vicpoke1);
-
-            $vicpoke2 = "select combates_ganados from pokemon where nombre like '$pokemon2'";
-            $resvicPoke2= $mysqli->query($vicpoke2);
-
-            $derrpoke1 = "select combates_perdidos from pokemon where nombre like '$pokemon1'";
-            $resderrpoke1 = $mysqli->query($derrpoke1);
-
-            $derrpoke2 = "select combates_perdidos from pokemon where nombre like '$pokemon2'";
-            $resderrpoke2 = $mysqli->query($derrpoke2);
             
             //Una vez obtenidas las victorias y derrotas de cada uno, actualizamos sus estadisticas.
             //Una vez tenemos las fuerzas comparamos los resultados
@@ -66,22 +61,27 @@
                 $ganador="Empate";
             } else if ($poder1>$poder2){
                 $ganador=$pokemon1;
-                $resvicPoke1+=1;
-                $resderrpoke2+=1;
+                $victorias1=$victorias1+1;
+                $derrotas2=$derrotas2+1;
+               
             } else {
                 $ganador=$pokemon2;
-                $resvicPoke2+=1;
-                $resderrpoke1+=1;
+                $derrotas1=$derrotas1+1;
+                $victorias2=$victorias2+1;
+            
             }
+            echo $victorias1, $derrotas1;
+            echo $victorias2, $derrotas2;
             //Elaboramos la sentencia SQL para insertar los datos en la base de datos
 		    $sql= "INSERT INTO combates(idpk1, idpk2, pokemon1, pokemon2, fecha, ganador) values('$idpk1', '$idpk2', '$pokemon1', '$pokemon2', '$fecha', '$ganador')";
 		    //Actualizamos los datos automáticamente mediante la sentencia.
-            $sqlActuPoke1 = "update pokemon set combates_ganados = $resvicPoke1, combates_perdidos = $resderrPoke1 where nombre like '$pokemon1';";
+            $sqlActuPoke1 = "UPDATE pokemon set combates_ganados = $victorias1, combates_perdidos = $derrotas1 where id like '$idpk1';";
 
-            $sqlActuPoke2 = "update pokemon set combates_ganados = $resvicPoke2, combates_perdidos = $resderrPoke2 where nombre like '$pokemon2';";
+            $sqlActuPoke2 = "UPDATE pokemon set combates_ganados = $victorias2, combates_perdidos = $derrotas2 where id like '$idpk2';";
             //Llevamos a cabo la consulta
 		    $res = $mysqli->query($sql);
-
+            $resActu1= $mysqli->query($sqlActuPoke1);
+            $resActu2=$mysqli->query($sqlActuPoke2);
 
 		    //Analizamos el resultado
     
@@ -93,7 +93,7 @@
                 echo "<p><a href='combate.php' class='btn btn-primary'>Regresar</a></p>";
             }
         }
-        
+    
 		?>
 
 	</body>
